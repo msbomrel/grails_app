00cd12ab7104801aa484a793af4cf92a6bbaf0df
msbomrel <mohan.bomrel@deerwalk.edu.np>
2016-06-18 17:12:42 +0545

    enquiry worked mainly

---
+++ grails-app/controllers/grails_app/EnquiryController.groovy
@@ -0,0 +1,26 @@
+package grails_app
+
+class EnquiryController {
+
+    def index() { }
+
+    def add(){
+        /*println "params = $params"
+        Enquiry enquiry=new Enquiry(params)
+        println "enquiry.service = $enquiry.service"
+        def service = Service.findById(params.service as long)
+        println "enquiry.serviceReqDate = $enquiry.serviceReqDate"*/
+       // enquiry.user=User.findById(1)
+        def service = Service.findById(params.service as long)
+        def enquiry = new Enquiry()
+        println params
+        enquiry.priority = params.priority
+        enquiry.details = params.details
+        enquiry.status="New"
+        enquiry.service=service
+        enquiry.serviceReqDate=params.serviceReqDate
+
+        enquiry.save(flush:true)
+        render ("Successfully Added")
+    }
+}
