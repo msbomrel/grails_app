f7bb31e8785d7a7774367fd3580c22f1da26c8f4
ruby-stha <stha.rubee@gmail.com>
2016-06-20 16:06:10 +0545

    Redirecting to different pages using logged in user's role

--- grails-app/controllers/grails_app/UserController.groovy
+++ grails-app/controllers/grails_app/UserController.groovy
@@ -1,8 +1,20 @@
 package grails_app
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.GrantedAuthority
 
 class UserController {
 
-    def index() {}
+    def index() {
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()
+        grails_app.User u=grails_app.User.findByUsername(user.getUsername())
+        request.getSession().setAttribute("userId", u.id);
+        if (user.authorities.any { it.authority == "ROLE_ADMIN" }){
+            render ("test")
+        }else {
+            render(view: "index")
+        }
+    }
 
     def save(){
         User user = new User(params)
